8e30cbc7bbc08c151676e61e285a942f170a05fb
oksana.safronova <oksana.aeshina666@gmail.com>
2022-06-29 15:53:04 +0300

    tasks 5-7

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template, send_from_directory, jsonify
 from main.views import main_blueprint
 from loader.views import loader_blueprint
 import functions
+import logging
 import json
 # from functions import ...
 
@@ -31,35 +32,32 @@ def page_post_form():
 
 @app.route("/post_uploaded/", methods=["POST"])
 def page_post_form():
-    text = request.values['content']
     picture = request.files.get('picture')
-    class NotInputError(Exception):
-        def __init__(self, message=None):
-            super().__init__(message)
-    if text == "None":
-        raise NotInputError("ошибка загрузки")
-    elif picture == "None":
-        raise NotInputError("ошибка загрузки")
+    if picture:
+        ALLOWED_EXT = {'png', 'jpg'}
+        filename = picture.filename
+        extension = filename.split(".")[-1]
+        if extension in ALLOWED_EXT:
+            picture.save(f"./{picture.filename}")
+            text = request.values['content']
+            class NotInputError(Exception):
+                def __init__(self, message=None):
+                    super().__init__(message)
+            if text == "None":
+                raise NotInputError("ошибка загрузки")
+            elif picture == "None":
+                raise NotInputError("ошибка загрузки")
+            else:
+                functions.upload_to_json_file()
+                return render_template("post_uploaded.html", text=text, picture=picture)
+        else:
+            logging.basicConfig(level=logging.INFO)
+            logging.info("Загруженный файл - не картинка")
+            return f"Тип файлов {extension} не поддерживается"
     else:
-        functions.upload_to_json_file()
-        return render_template("post_uploaded.html", text=text, picture=picture)
-
-
-
-
-
-
-
-
-
-
-@app.route("/post", methods=["GET", "POST"])
-def page_post_form():
-    pass
-
-@app.route("/post", methods=["POST"])
-def page_post_upload():
-    pass
+        logging.basicConfig(level=logging.ERROR)
+        logging.error("ошибка при загрузке файла")
+        return f"Ошибка при загрузке файла"
 
 
 @app.route("/uploads/<path:path>")
--- functions.py
+++ functions.py
@@ -1,14 +1,25 @@
+from json import JSONDecodeError
+import logging
 from flask import Flask, request, render_template, send_from_directory, jsonify
 import json
 from pprint import pp
 
-f = open("posts.json")
-data_ = json.load(f)
+try:
+    f = open("posts.json")
+    data_ = json.load(f)
+except FileNotFoundError:
+    print('Файл posts.json отсутствует')
+except JSONDecodeError:
+    print('Файл posts.json не хочет превращаться в список')
+
+
 
 def search_posts(s):
     posts_found = []
     if s in data_['content'].lower():
         posts_found.append(data_)
+    logging.basicConfig(level=logging.INFO)
+    logging.info("Поиск выполнен")
     return posts_found
 
 def upload_to_json_file():
